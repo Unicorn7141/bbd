7c574a1d78499316e76944a5dbf496a5c9b000ec
Unicorn7141 <harelibr@gmail.com>
2025-11-22 19:20:14 +0200

    v1.0.3

--- components/Chart.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Chart() {
-  return (
-    <div className="h-60 flex items-center justify-center text-neutral-500">
-      (Chart placeholder)
-    </div>
-  );
-}
